26820dab07e77fe6cd84fef3211ab2bc69e2ea18
Edimar Soares <104039279+ESCSOARES@users.noreply.github.com>
2023-01-11 13:17:27 -0300

    Add files via upload

---
+++ gerador_email_e_senha.py
@@ -0,0 +1,57 @@
+import string
+from random import random, choice
+
+print ('qual o nome do email você quer?')
+corpo_email = input ()
+print('escolha números para o email (possa ser que já exista um com o mesmo nome, mas não com números iguais)')
+numero= input()
+print ('qual o tipo de extensão você quer?')
+print('1- gmail.com   2- hotmail.com   3- protonmail.com')
+extensao_email = input()
+if extensao_email == "1":
+    print(corpo_email + numero + "@gmail.com" )
+
+    tamanho = 8
+    combinacoes = string.ascii_letters
+    combinacoes += string.digits
+    combinacoes += string.punctuation
+    tamanho_senha = tamanho
+    senha = ""
+
+    for i in range(tamanho):
+        senha += choice(combinacoes)
+
+    print('aa senha desse email é :', senha)
+    input('')
+
+if extensao_email == "2":
+    print(corpo_email + numero + "@hotmail.com" )
+
+    tamanho = 8
+    combinacoes = string.ascii_letters
+    combinacoes += string.digits
+    combinacoes += string.punctuation
+    tamanho_senha = tamanho
+    senha = ""
+
+    for i in range(tamanho):
+        senha += choice(combinacoes)
+
+    print('a senha desse email é :', senha)
+    input('')
+#
+if extensao_email == "3":
+    print(corpo_email + numero + "@protonmail.com" )
+
+    tamanho = 8
+    combinacoes = string.ascii_letters
+    combinacoes += string.digits
+    combinacoes += string.punctuation
+    tamanho_senha = tamanho
+    senha = ""
+
+    for i in range(tamanho):
+        senha += choice(combinacoes)
+
+    print('a senha desse email é :', senha)
+    input('')
